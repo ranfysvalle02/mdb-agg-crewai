f678029776bbad9716962794013cd66e23c970a0
Fabian Valle <ranfys.valle@gmail.com>
2024-06-05 11:14:03 -0400

    Update investment_analysis.py

--- investment_analysis.py
+++ investment_analysis.py
@@ -1,47 +1,59 @@
 import os
 import pymongo
+import pprint 
 
+# MongoDB Setup
 MDB_URI = "mongodb+srv://<user>:<password>@cluster0.abc123.mongodb.net/"
 client = pymongo.MongoClient(MDB_URI)
 db = client["sample_analytics"]
 collection = db["transactions"]
 
+# Azure OpenAI Setup
 from langchain_openai import AzureChatOpenAI
-
 AZURE_OPENAI_ENDPOINT = "https://__DEMO__.openai.azure.com"
-AZURE_OPENAI_API_KEY = "__AZURE_OPENAI_API_KEY__" 
+AZURE_OPENAI_API_KEY = "__AZURE_OPENAI_API_KEY__"
 deployment_name = "gpt-4-32k"  # The name of your model deployment
 default_llm = AzureChatOpenAI(
-    openai_api_version=os.environ.get("AZURE_OPENAI_VERSION", "2023-07-01-preview"),
-    azure_deployment=deployment_name,
-    azure_endpoint=AZURE_OPENAI_ENDPOINT,
-    api_key=AZURE_OPENAI_API_KEY
+	openai_api_version=os.environ.get("AZURE_OPENAI_VERSION", "2023-07-01-preview"),
+	azure_deployment=deployment_name,
+	azure_endpoint=AZURE_OPENAI_ENDPOINT,
+	api_key=AZURE_OPENAI_API_KEY
 )
 
-from langchain_community.utilities import GoogleSerperAPIWrapper
-from langchain.agents import Tool
+# Web Search Setup
+from langchain.tools import tool
+from langchain_community.tools import DuckDuckGoSearchResults
+duck_duck_go = DuckDuckGoSearchResults(backend="news",max_results=10)
 
-search = GoogleSerperAPIWrapper(serper_api_key='__API_KEY__')
-search_tool = Tool(
-        name="Google Answer",
-        func=search.run,
-        description="useful for when you need to ask with search"
-    )
+# Search Tool - Web Search
+@tool
+def search_tool(query: str):
+  """
+  Perform online research on a particular stock.
+  Will return search results along with snippets of each result.
+  """
+  print("\n\nSearching DuckDuckGo for:", query)
+  search_results = duck_duck_go.run(query)
+  search_results_str =  "[recent news for: " + query + "]\n" + str(search_results)
+  return search_results_str
 
+
+# Research Agent Setup
 from crewai import Crew, Process, Task, Agent
-
+AGENT_ROLE = "Investment Researcher"
+AGENT_GOAL = """
+  Research stock market trends, company news, and analyst reports to identify potential investment opportunities.
+"""
 researcher = Agent(
-  role='Investment Researcher',
-  goal="""
-  Research market trends, company news, and analyst reports to identify potential investment opportunities.
-  """,
+  role=AGENT_ROLE,
+  goal=AGENT_GOAL,
   verbose=True,
   llm=default_llm,
-  backstory='Expert in using search engines to uncover relevant financial data, news articles, and industry analysis.',
+  backstory='Expert stock researcher with decades of experience.',
   tools=[search_tool]
 )
 
-analysis_task = Task(
+task1 = Task(
   description="""
 Using the following information:
 
@@ -49,101 +61,84 @@ Using the following information:
 {agg_data}
 
 *note*
-The data represents the average price of each stock symbol for each transaction type (buy/sell),
-and the total amount of transactions for each type. This would give us insight into the average costs and proceeds from each stock,
-as well as the volume of transactions for each stock.
+The data represents the net gain or loss of each stock symbol for each transaction type (buy/sell).
+Net gain or loss is a crucial metric used to gauge the profitability or efficiency of an investment. 
+It's computed by subtracting the total buy value from the total sell value for each stock.
 [END VERIFIED DATA]
 
 [TASK]
-- Provide a financial summary of the VERIFIED DATA
-- Research current events and trends, and provide actionable insights and recommendations
+- Generate a detailed financial report of the VERIFIED DATA.
+- Research current events and trends, and provide actionable insights and recommendations.
+
+
+[report criteria]
+  - Use all available information to prepare this final financial report
+  - Include a TLDR summary
+  - Include 'Actionable Insights'
+  - Include 'Strategic Recommendations'
+  - Include a 'Other Observations' section
+  - Include a 'Conclusion' section
+  - IMPORTANT! You are a friendly and helpful financial expert. Always provide the best possible answer using the available information.
+[end report criteria]
   """,
   agent=researcher,
-  expected_output='concise markdown financial summary and list of actionable insights and recommendations',
+  expected_output='concise markdown financial summary of the verified data and list of key points and insights from researching current events',
   tools=[search_tool],
 )
-
+# Crew Creation
 tech_crew = Crew(
   agents=[researcher],
-  tasks=[analysis_task],
+  tasks=[task1],
   process=Process.sequential
 )
 
+# MongoDB Aggregation Pipeline
 pipeline = [
-  {"$unwind": "$transactions"},
-  {"$group": {
-      "_id": "$transactions.symbol",
-      "buyValue": {
-        "$sum": {
-          "$cond": [
-            { "$eq": ["$transactions.transaction_code", "buy"] },
-            { "$toDouble": "$transactions.total" },
-            0
-          ]
-        }
+  {
+    "$unwind": "$transactions"  # Deconstruct the transactions array into separate documents
+  },
+  {
+    "$group": {             		 # Group documents by stock symbol
+      "_id": "$transactions.symbol",  # Use symbol as the grouping key
+      "buyValue": {           		 # Calculate total buy value
+   	 "$sum": {
+ 		 "$cond": [          		 # Conditional sum based on transaction type
+   		 { "$eq": ["$transactions.transaction_code", "buy"] },  # Check for "buy" transactions
+   		 { "$toDouble": "$transactions.total" },      		 # Convert total to double for sum
+   		 0                                         		 # Default value for non-buy transactions
+ 		 ]
+   	 }
       },
-      "sellValue": {
-        "$sum": {
-          "$cond": [
-            { "$eq": ["$transactions.transaction_code", "sell"] },
-            { "$toDouble": "$transactions.total" },
-            0
-          ]
-        }
+      "sellValue": {          		 # Calculate total sell value (similar to buyValue)
+   	 "$sum": {
+ 		 "$cond": [
+   		 { "$eq": ["$transactions.transaction_code", "sell"] },
+   		 { "$toDouble": "$transactions.total" },
+   		 0
+ 		 ]
+   	 }
       }
     }
   },
-  {"$project": {
-      "_id": 0,
-      "symbol": "$_id",
-      "returnOnInvestment": { "$subtract": ["$sellValue", "$buyValue"] }
+  {
+    "$project": {            		 # Project desired fields (renaming and calculating net gain)
+      "_id": 0,               		 # Exclude original _id field
+      "symbol": "$_id",        		 # Rename _id to symbol for clarity
+      "netGain": { "$subtract": ["$sellValue", "$buyValue"] }  # Calculate net gain
     }
   },
-  {"$sort": { "returnOnInvestment": -1 }}
+  {
+    "$sort": { "netGain": -1 }  # Sort results by net gain (descending)
+  },
+  {"$limit": 3}  # Limit results to top 3 stocks 
 ]
 results = list(collection.aggregate(pipeline))
 client.close()
 
+# Print MongoDB Aggregation Pipeline Results
 print("MongoDB Aggregation Pipeline Results:")
-print(results)
 
+pprint.pprint(results) #pprint is used to  to “pretty-print” arbitrary Python data structures
+
+# Start the task execution
 tech_crew.kickoff(inputs={'agg_data': str(results)})
-
-# OUTPUT:
-"""
-Thought: I now know the final answer
-Final Answer:
-
-Financial Summary:
-
-From the verified data, we can see that the following stocks have generated the highest returns on investment:
-
-1. Amazon (AMZN): 72,769,230.71
-2. SAP: 39,912,931.05
-3. Apple (AAPL): 25,738,882.29
-4. Adobe (ADBE): 17,975,929.73
-
-On the other hand, the following stocks have produced negative returns:
-
-1. Atlassian Corporation Plc (TEAM): -406,507.08
-2. Netflix (NFLX): -2,133,963.99
-3. Intel Corporation (INTC): -7,407,861.20
-4. Salesforce (CRM): -15,106,640.20
-5. Microsoft (MSFT): -15,665,720.74
-6. IBM: -18,356,948.23
-7. Google (GOOG): -168,114,276.92
-
-Actionable Insights and Recommendations:
-
-Based on current news and events:
-
-1. Amazon (AMZN): The company's stock is performing strongly, approaching its record high of 2021. It is recommended to keep an eye on this stock for potential investment opportunities.
-
-2. SAP: Despite the company's plan to cut jobs, analysts have a positive outlook on the stock. It is advisable to monitor the stock closely for any changes in its performance.
-
-3. Apple (AAPL): The recent surge in the company's stock value due to its intention to delve into AI shows promising growth. Investors may consider this stock for potential investment.
-
-4. Adobe (ADBE): The stock is showing a positive sentiment in relation to other stocks in the technology sector and has a high price target set by Wall Street analysts. It is recommended to watch this stock for potential growth.
-
-It is also suggested to keep a close eye on those stocks that are currently showing negative returns. Understanding the reasons behind their poor performance could provide investment opportunities if these issues are addressed and the businesses start to turn around.
-"""
